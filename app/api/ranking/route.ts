46ed314c522dd81cfd66e7b59e692e953e7c3f02
CatsEleven <hatijolove@gmail.com>
2025-02-19 00:09:53 +0900

    feat: ranking-api

---
+++ app/api/ranking/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server"
+import { prisma } from "@/lib/prisma"
+
+const GET = async () => {
+    const post = await prisma.post.findMany({
+        where: {
+            isDraft: false
+        },
+        take: 3,
+        orderBy: {
+            likes: {
+                _count: "desc",
+            },
+        },
+    })
+
+    return NextResponse.json(post, { status: 200 })
+}
+
+export default GET
