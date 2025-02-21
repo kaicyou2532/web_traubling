c6f8864dcc6704470e337ca3bb3ad27e6b8a48fe
U-Yuna <a5823016@aoyama.jp>
2025-02-21 23:39:20 +0900

    Sheet

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,72 +3,96 @@ import {
   UserCircleIcon,
   MapIcon,
   MagnifyingGlassIcon,
-  ShareIcon,
+  PencilSquareIcon,
+  GlobeAsiaAustraliaIcon
 } from "@heroicons/react/24/solid"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { Bars3Icon } from "@heroicons/react/24/solid"
 import { AuthModal } from "../components/login"
 
 export default function Header() {
-
   return (
-    <header className="bg-white shadow-md">
+      <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link href="/" className="text-2xl font-extrabold text-custom-green">
           traubling
         </Link>
-        <nav>
-          <ul className="flex flex-wrap gap-4 items-center">
-            <li>
-              <Link
-                href="/"
-                className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
-              >
-                <MagnifyingGlassIcon className="h-5 w-5" />
-                <span>探す</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/cities/international"
-                className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
-              >
-                <MapIcon className="h-5 w-5" />
-                <span>海外で気をつけること</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/cities/japan"
-                className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
-              >
-                <MapIcon className="h-5 w-5" />
-                <span>国内で気をつけること</span>
-              </Link>
-            </li>
-            {/* <li>
-              <Link
-                href="/consult"
-                className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
-              >
-                <ChatBubbleLeftRightIcon className="h-5 w-5" />
-                <span>相談する</span>
-              </Link>
-            </li> */}
-            <li>
-              <Link
-                href="/post"
-                className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
-              >
-                <ShareIcon className="h-5 w-5" />
-                <span>トラブルを共有する</span>
-              </Link>
-            </li>
-            <AuthModal>
-              <button className="text-gray-700 hover:text-custom-green transition-colors">
-                <UserCircleIcon className="h-8 w-8" />
-              </button>
-            </AuthModal>
-          </ul>
-        </nav>
+        
+      <Sheet>
+        <SheetTrigger asChild>
+            <Bars3Icon className="h-8 w-8 text-gray-700" />
+        </SheetTrigger>
+        <SheetContent className="bg-white">
+          <nav>
+            <ul className="flex flex-col gap-4 items-start">
+              <li>
+                <SheetClose asChild>
+                  <Link
+                    href="/"
+                    className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
+                  >
+                    <MagnifyingGlassIcon className="h-7 w-7" />
+                    <span>探す</span>
+                  </Link>
+                </SheetClose>
+              </li>
+              <li>
+                <SheetClose asChild>
+                  <Link
+                    href="/cities/international"
+                    className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
+                  >
+                    <GlobeAsiaAustraliaIcon className="h-7 w-7" />
+                    <span>海外で気をつけること</span>
+                  </Link>
+                </SheetClose>
+              </li>
+              <li>
+                <SheetClose asChild>
+                  <Link
+                    href="/cities/japan"
+                    className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
+                  >
+                    <MapIcon className="h-7 w-7" />
+                    <span>国内で気をつけること</span>
+                  </Link>
+                </SheetClose>
+              </li>
+              <li>
+                <SheetClose asChild>
+                  <Link
+                    href="/post"
+                    className="text-gray-700 hover:text-custom-green transition-colors font-medium flex items-center gap-2"
+                  >
+                    <PencilSquareIcon className="h-7 w-7" />
+                    <span>トラブルを共有する</span>
+                  </Link>
+                </SheetClose>
+              </li>
+              <li>
+                <SheetClose asChild>
+                  <AuthModal>
+                    <button className="text-gray-700 hover:text-custom-green transition-colors flex items-center gap-2">
+                      <UserCircleIcon className="h-7 w-7" />
+                      <span>ログイン</span>
+                    </button>
+                  </AuthModal>
+                </SheetClose>
+              </li>
+            </ul>
+          </nav>
+        </SheetContent>
+      </Sheet>
       </div>
     </header>
   )
