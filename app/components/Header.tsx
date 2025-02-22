622c32a6051a17334c51f336e1545fe08026ca2a
U-Yuna <a5823016@aoyama.jp>
2025-02-22 19:22:56 +0900

    changed header title

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -26,7 +26,7 @@ export default async function Header() {
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link href="/" className="text-2xl font-extrabold text-custom-green">
-          traubling
+          Traubling
         </Link>
 
         <div className="flex justify-center items-center gap-3">
