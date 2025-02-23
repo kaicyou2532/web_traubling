eae162e342d4c90cf9057cc779e6dc9e850f398a
balckowl <shiggyjr1492@gmail.com>
2025-02-23 13:42:00 +0900

    feat: google認証を追加

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
+import GOOGLE from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./lib/prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Discord],
+  providers: [Discord, GOOGLE],
 })
